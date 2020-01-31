175794546c1899da11b01751f0ec8cdadfc9d673
RodrigoSilvaMartins <rsmartinspro@gmail.com>
2020-01-31 04:26:23 -0800

    DB, Entities & Repositories setup, last changements/PR Review

--- src/Migrations/Version20200131122424.php
+++ src/Migrations/Version20200131122424.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200131105110 extends AbstractMigration
+final class Version20200131122424 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,15 +22,15 @@ final class Version20200131105110 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE t_subChapters (id INT AUTO_INCREMENT NOT NULL, subName VARCHAR(255) NOT NULL, subNumber INT NOT NULL, fkChapter INT NOT NULL, fkLevel INT DEFAULT NULL, INDEX IDX_6596B8AFEFA103A1 (fkChapter), INDEX IDX_6596B8AF8A304D32 (fkLevel), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE t_subChapters (idSubChapter INT AUTO_INCREMENT NOT NULL, subName VARCHAR(255) NOT NULL, subNumber INT NOT NULL, fkChapter INT NOT NULL, fkLevel INT DEFAULT NULL, INDEX IDX_6596B8AFEFA103A1 (fkChapter), INDEX IDX_6596B8AF8A304D32 (fkLevel), PRIMARY KEY(idSubChapter)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE t_subjects (idSubject INT AUTO_INCREMENT NOT NULL, subName VARCHAR(255) NOT NULL, PRIMARY KEY(idSubject)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE t_levels (idLevel INT AUTO_INCREMENT NOT NULL, levName VARCHAR(255) NOT NULL, PRIMARY KEY(idLevel)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE t_image (idImage INT AUTO_INCREMENT NOT NULL, imaName VARCHAR(255) NOT NULL, imaContent LONGBLOB NOT NULL, imaFormat VARCHAR(255) NOT NULL, PRIMARY KEY(idImage)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE t_examQuestions (idExamQuestion INT AUTO_INCREMENT NOT NULL, exaOrder INT NOT NULL, fkExam INT NOT NULL, fkQuestion INT NOT NULL, INDEX IDX_409159E43840DD5C (fkExam), INDEX IDX_409159E4CBE6F749 (fkQuestion), PRIMARY KEY(idExamQuestion)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE t_themes (idTheme INT AUTO_INCREMENT NOT NULL, theName VARCHAR(255) NOT NULL, fkSubject INT NOT NULL, INDEX IDX_3C254A20EDEE88F5 (fkSubject), PRIMARY KEY(idTheme)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE t_exams (idExam INT AUTO_INCREMENT NOT NULL, exaName VARCHAR(255) NOT NULL, exaCreationDate VARCHAR(255) DEFAULT NULL, duration VARCHAR(255) NOT NULL, fkSubject INT NOT NULL, INDEX IDX_D7807551EDEE88F5 (fkSubject), PRIMARY KEY(idExam)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE t_exams (idExam INT AUTO_INCREMENT NOT NULL, exaName VARCHAR(255) NOT NULL, exaCreationDate DATETIME DEFAULT NULL, duration VARCHAR(255) NOT NULL, fkSubject INT NOT NULL, INDEX IDX_D7807551EDEE88F5 (fkSubject), PRIMARY KEY(idExam)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE t_chapters (idChapter INT AUTO_INCREMENT NOT NULL, chaName VARCHAR(255) NOT NULL, chaNumber INT NOT NULL, fkTheme INT NOT NULL, INDEX IDX_13C05F2B87AF6629 (fkTheme), PRIMARY KEY(idChapter)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE t_questions (idQuestion INT AUTO_INCREMENT NOT NULL, fkSubChapter INT DEFAULT NULL, queQuestion VARCHAR(255) NOT NULL, queAnswer VARCHAR(255) NOT NULL, quePoints INT NOT NULL, queFormula LONGTEXT DEFAULT NULL, fkImage INT DEFAULT NULL, INDEX IDX_1B60D23D5E7857E (fkImage), PRIMARY KEY(idQuestion)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE t_questions (idQuestion INT AUTO_INCREMENT NOT NULL, queQuestion VARCHAR(255) NOT NULL, queAnswer VARCHAR(255) NOT NULL, quePoints INT NOT NULL, queFormula LONGTEXT DEFAULT NULL, fkSubChapter INT DEFAULT NULL, fkImage INT DEFAULT NULL, INDEX IDX_1B60D23D5E7857E (fkImage), PRIMARY KEY(idQuestion)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE t_subChapters ADD CONSTRAINT FK_6596B8AFEFA103A1 FOREIGN KEY (fkChapter) REFERENCES t_chapters (idChapter)');
         $this->addSql('ALTER TABLE t_subChapters ADD CONSTRAINT FK_6596B8AF8A304D32 FOREIGN KEY (fkLevel) REFERENCES t_levels (idLevel)');
         $this->addSql('ALTER TABLE t_examQuestions ADD CONSTRAINT FK_409159E43840DD5C FOREIGN KEY (fkExam) REFERENCES t_exams (idExam)');
