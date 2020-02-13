27c26d9046807989c014bc1da9f7e3a509ce64fa
RodrigoSilvaMartins <rsmartinspro@gmail.com>
2020-02-13 00:02:39 -0800

    Changed api structure

---
+++ src/Controller/Back/ApiController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Repository\QuestionRepository;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/api/question.list", name="getQuestionsList", methods={"GET"})
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns a list of questions",
+     *     @Model(type=QuestionView::class)
+     * )
+     * @SWG\Parameter(
+     *     name="body",
+     *     in="body",
+     *     description="Questions filters",
+     *     required=false,
+     *     @SWG\Schema(
+     *     @SWG\Items(
+     *            type="object",
+     *        	@SWG\Property(property="questionsId", type="array", @SWG\Items(type="integer")),
+     *         	@SWG\Property(property="subChaptersId", type="array", @SWG\Items(type="integer")),
+     *     ),
+     *     )
+     * )
+     *
+     * @SWG\Tag(name="Questions")
+     */
+    public function getQuestionsList(Request $request, QuestionRepository $repository): Response
+    {
+        return new Response($this->get('serializer')->serialize($repository->list(
+            $request->request->get('questionsId'),
+            $request->request->get('subChaptersId')
+        ), 'json'));
+    }
+}
--- src/Controller/Front/MainController.php
+++ src/Controller/Front/MainController.php
@@ -2,14 +2,8 @@
 
 namespace App\Controller\Front;
 
-use App\Repository\QuestionRepository;
-use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\View\QuestionView;
-use Swagger\Annotations as SWG;
 
 class MainController extends AbstractController
 {
@@ -18,37 +12,15 @@ class MainController extends AbstractController
      */
     public function landingPage()
     {
+//        $httpClient = HttpClient::create();
+//        $response = $httpClient->request('GET', 'api/question.list');
+//        $content = $response->toArray();
         return $this->render('landingPage.html.twig', ['hello' => 'VIKI']);
     }
-
     /**
-     * @Route("/api/question.list", name="getQuestionsList", methods={"GET"})
-     * @SWG\Response(
-     *     response=200,
-     *     description="Returns a list of questions",
-     *     @Model(type=QuestionView::class)
-     * )
-     * @SWG\Parameter(
-     *     name="body",
-     *     in="body",
-     *     description="Questions filters",
-     *     required=false,
-     *     @SWG\Schema(
-     *     @SWG\Items(
-     *            type="object",
-     *        	@SWG\Property(property="questionsId", type="array", @SWG\Items(type="integer")),
-     *         	@SWG\Property(property="subChaptersId", type="array", @SWG\Items(type="integer")),
-     *     ),
-     *     )
-     * )
-     *
-     * @SWG\Tag(name="Questions")
+     * @Route("/questions/", name ="questions_page")
      */
-    public function getQuestionsList(Request $request, QuestionRepository $repository): Response
-    {
-        return new Response($this->get('serializer')->serialize($repository->list(
-            $request->request->get('questionsId'),
-            $request->request->get('subChaptersId')
-        ), 'json'));
+    public function  questionsPage(){
+        return $this->render('questions.html.twig', ['title' => 'Questions']);
     }
 }
