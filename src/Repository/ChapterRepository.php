7b371c1a80ec6d88fc7168570ea51fe3e36b74ad
Monnierta <33867270+Monnierta@users.noreply.github.com>
2020-02-27 00:59:31 -0800

    Update ChapterRepository.php

--- src/Repository/ChapterRepository.php
+++ src/Repository/ChapterRepository.php
@@ -3,7 +3,6 @@
 namespace App\Repository;
 
 use App\Entity\Chapter;
-use App\Entity\Question;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
